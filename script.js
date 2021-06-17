4b9c115e686bb128c60221441210ee24ef28492a
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-06-17 12:16:12 +0200

    Footer edits, mobile and desktop

---
+++ script.js
@@ -0,0 +1,9 @@
+function popForm(){
+    const popUpContact = document.getElementById('contact-drop');
+
+    if (popUpContact.style.display === "none"){
+        popUpContact.style.display = "block";
+    } else {
+        popUpContact.style.display = "none";
+        }
+    }
